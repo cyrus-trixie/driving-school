c46faecb6a8c35369c8cf01375141df6585d60d8
cyrus-trixie <125303529+cyrus-trixie@users.noreply.github.com>
2025-07-29 18:50:16 +0300

    final

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,24 +1,2 @@
 
-import CakeNavigation from "@/components/CakeNavigation";
-import CakeHero from "@/components/CakeHero";
-import CakeWorkDone from "@/components/CakeWorkDone";
-import CakeGallery from "@/components/CakeGallery";
-import CakeAbout from "@/components/CakeAbout";
-import CakeTypes from "@/components/CakeTypes";
-import CakeContact from "@/components/CakeContact";
-
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-pastel-cream to-pastel-pink">
-      <CakeNavigation />
-      <CakeHero />
-      <CakeWorkDone />
-      <CakeGallery />
-      <CakeAbout />
-      <CakeTypes />
-      <CakeContact />
-    </div>
-  );
-};
-
-export default Index;
+export default function Index() { return null; }
